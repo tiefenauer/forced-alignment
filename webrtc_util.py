6b7dfad45d2f4f38cefcfd29ddbd7d1de60f938d
Daniel Tiefenauer <git@tiefenauer.info>
2018-05-09 17:43:41 +0200

    create webrtc_util

--- webrtc_util.py
+++ webrtc_util.py
@@ -2,57 +2,42 @@ import contextlib
 import sys
 import wave
 
-import collections
-import webrtcvad
-
-from corpus_util import load_corpus
+from webrtcvad import Vad
 
 
 class Frame(object):
-    """Represents a "frame" of audio data."""
-
     def __init__(self, bytes, timestamp, duration):
         self.bytes = bytes
         self.timestamp = timestamp
         self.duration = duration
 
 
-def frame_generator(frame_duration_ms, audio, sample_rate):
-    """Generates audio frames from PCM audio data.
-    Takes the desired frame duration in milliseconds, the PCM data, and
-    the sample rate.
-    Yields Frames of the requested duration.
-    """
-    n = int(sample_rate * (frame_duration_ms / 1000.0) * 2)
+def get_segments(corpus_entry, vad=Vad(3)):
+    rate, audio = corpus_entry.audio
+    print(f'sample rate: {rate}, audio: {audio.shape}')
+    frames = generate_frames(audio, rate)
+    frames = list(frames)
+    print(f'frames: {len(frames)}')
+    segments = vad_collector(rate, vad, frames)
+    for i, segment in enumerate(segments):
+        path = 'chunk-%002d.wav' % (i,)
+        print(' Writing %s' % (path,))
+        # write_wav_file(path, sample_rate, segment)
+        write_wave(path, segment, rate)
+
+
+def generate_frames(audio, rate, frame_duration_ms=30):
+    n = int(rate * (frame_duration_ms / 1000.0) * 2)
     offset = 0
     timestamp = 0.0
-    duration = (float(n) / sample_rate) / 2.0
+    duration = (float(n) / rate) / 2.0
     while offset + n < len(audio):
         yield Frame(audio[offset:offset + n], timestamp, duration)
         timestamp += duration
         offset += n
 
 
-def vad_collector(sample_rate, frame_duration_ms, padding_duration_ms, vad, frames):
-    """Filters out non-voiced audio frames.
-    Given a webrtcvad.Vad and a source of audio frames, yields only
-    the voiced audio.
-    Uses a padded, sliding window algorithm over the audio frames.
-    When more than 90% of the frames in the window are voiced (as
-    reported by the VAD), the collector triggers and begins yielding
-    audio frames. Then the collector waits until 90% of the frames in
-    the window are unvoiced to detrigger.
-    The window is padded at the front and back to provide a small
-    amount of silence or the beginnings/endings of speech around the
-    voiced frames.
-    Arguments:
-    sample_rate - The audio sample rate, in Hz.
-    frame_duration_ms - The frame duration in milliseconds.
-    padding_duration_ms - The amount to pad the window, in milliseconds.
-    vad - An instance of webrtcvad.Vad.
-    frames - a source of audio frames (sequence or generator).
-    Returns: A generator that yields PCM audio data.
-    """
+def vad_collector(sample_rate, vad, frames, frame_duration_ms=30, padding_duration_ms=0):
     num_padding_frames = int(padding_duration_ms / frame_duration_ms)
     # We use a deque for our sliding window/ring buffer.
     ring_buffer = collections.deque(maxlen=num_padding_frames)
@@ -113,27 +98,3 @@ def write_wave(path, audio, sample_rate):
         wf.setsampwidth(2)
         wf.setframerate(sample_rate)
         wf.writeframes(audio)
-
-
-def main():
-    vad = webrtcvad.Vad(3)
-
-    rl_corpus = load_corpus(r'E:\readylingua-corpus\readylingua.corpus')
-    # corpus_entry = random.choice(rl_corpus)
-    corpus_entry = rl_corpus[0]
-
-    sample_rate, audio = corpus_entry.audio
-    print(f'sample rate: {sample_rate}, audio: {audio.shape}')
-    frames = frame_generator(30, audio, sample_rate)
-    frames = list(frames)
-    print(f'frames: {len(frames)}')
-    segments = vad_collector(sample_rate, 30, 30, vad, frames)
-    for i, segment in enumerate(segments):
-        path = 'chunk-%002d.wav' % (i,)
-        print(' Writing %s' % (path,))
-        # write_wav_file(path, sample_rate, segment)
-        write_wave(path, segment, sample_rate)
-
-
-if __name__ == '__main__':
-    main()

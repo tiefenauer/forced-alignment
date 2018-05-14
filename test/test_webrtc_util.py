ff449a92c2e33d39fc4da1095e2b654cb7177c39
Daniel Tiefenauer <git@tiefenauer.info>
2018-05-14 12:16:03 +0200

    move webrtc.py to test_webrtc_util.py

--- test/test_webrtc_util.py
+++ test/test_webrtc_util.py
@@ -1,12 +1,26 @@
+import os
 from unittest import TestCase
+
+import numpy as np
 
 import corpus_util
 import webrtc_util
+from audio_util import write_wav_file
 
 
 class TestWebRtcUtil(TestCase):
 
     def test_split_into_segments(self):
         rl_corpus = corpus_util.load_corpus(r'E:\readylingua-corpus\readylingua.corpus')
+        corpus_entry = rl_corpus[0]
+
+        sample_rate, audio = corpus_entry.audio
+        voiced_segments, unvoiced_segments = webrtc_util.split_segments(corpus_entry)
+        for i, voiced_segment in enumerate(voiced_segments):
+            audio = np.concatenate([frame.audio for frame in voiced_segment])
+            file_path = os.path.join('..', f'chunk-{i:0002d}.wav')
+            print(f'Writing {file_path}')
+            write_wav_file(file_path, sample_rate, audio)
+
         corpus_entry = rl_corpus[0]
         webrtc_util.split_segments(corpus_entry)

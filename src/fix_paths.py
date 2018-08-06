61b77fedfd62583f51807200c0ab748d30e5ed5f
Daniel Tiefenauer <git@tiefenauer.info>
2018-08-06 10:09:17 +0200

    load corpora from arbitrary corpus roots

--- src/fix_paths.py
+++ src/fix_paths.py
@@ -2,20 +2,26 @@
 This file can be used if a corpus was created on the cluster (Unix) and downloaded locally (Windows) or vice versa.
 Since the paths to the audio files are absolute, this script will fix them.
 """
+from util.log_util import create_args_str
 
-# set corpus ID here
+parser = argparse.ArgumentParser(
+    description="""change paths of corpus entries to new root""")
+parser.add_argument('corpus_id', type=str, choices=['rl', 'ls'], help='corpus to use (rl=ReadyLingua, ls=LibriSpeech')
+parser.add_argument('new_root', type=str, help=f'new root path of')
+args = parser.parse_args()
+
 from os.path import basename, join
 
 from tqdm import tqdm
 
-from constants import RL_CORPUS_ROOT, LS_CORPUS_ROOT
 from util.corpus_util import get_corpus, save_corpus
 
-corpus_id = 'ls'
-
 if __name__ == '__main__':
-    corpus = get_corpus(corpus_id)
-    corpus.root_path = LS_CORPUS_ROOT if corpus_id == 'ls' else RL_CORPUS_ROOT
+    print(create_args_str(args))
+    corpus = get_corpus(args.corpus_id)
+    print(f'changing corpus root from {corpus.root_path} to {args.new_root}')
+    corpus.root_path = args.new_root
+
     progress = tqdm(corpus, unit=' corpus entries')
     for corpus_entry in progress:
         old_path = corpus_entry.audio_file
